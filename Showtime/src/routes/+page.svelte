68193fa2fe4520f94ea47f38b19ae7b5b1bba048
Viro-Neo <van-do.pham@epitech.eu>
2025-01-06 12:08:16 +0100

    :sparkles: add listing from json file

--- Showtime/src/routes/+page.svelte
+++ Showtime/src/routes/+page.svelte
@@ -1,19 +1,38 @@
 <script lang="ts">
+    import type { Project } from '$lib/types';
     import projects from './projects/projects.json';
+
+    const typedProjects: Project[] = projects;
 </script>
 
 <h1>Homepage</h1>
 <h2>Welcome to Showtime, Van-Do PHAM's personal portfolio</h2>
 <h2>Projects</h2>
+<ul>
+    {#each typedProjects as project}
+        <li>{project.name}: {project.description}</li>
+    {/each}
+</ul>
 
 <style>
-  h1 {
-      padding-top: 5%;
-      text-align: center;
-      padding-bottom: 5%;
-  }
-  h2 {
-      text-align: center;
-      padding-bottom: 5%;
-  }
+    h1 {
+        padding-top: 5%;
+        text-align: center;
+        padding-bottom: 5%;
+    }
+
+    h2 {
+        text-align: center;
+        padding-bottom: 5%;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+        text-align: center;
+    }
+
+    li {
+        padding: 5px 0;
+    }
 </style>
